76a05204d5c7a98f3176f860a2fbe508a0e30bda
Margo Kovalenko <Tyuiu.KovalenkoM@outlook.com>
2025-09-14 13:08:22 +0500

    Task1

---
+++ Tyuiu.KovalenkoMA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Margo";
+        int age = 17;
+        Console.WriteLine("Hello >.< My name is " + name + " I am " + age + " years old");
+    }
+}
