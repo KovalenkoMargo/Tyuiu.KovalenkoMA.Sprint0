86348cbc68db33ccc0934a02432ec11574e441bd
Margo Kovalenko <Tyuiu.KovalenkoM@outlook.com>
2025-09-14 16:22:58 +0500

    Task2

---
+++ Tyuiu.KovalenkoMA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KovalenkoMA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.KovalenkoMA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Margo";
+            var res = DataService.GetMessage(name);
+
+            // Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Hello, Margo", res);
+        }
+    }
+}
---
+++ Tyuiu.KovalenkoMA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.KovalenkoMA.Sprint0.Task2.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Вызов класса DataService и метода GetMessage
+        // из библиотеки Tyuiu.KovalenkoMA.Sprint0.Task2.V0.Lib
+        Console.WriteLine(DataService.GetMessage("Margo"));
+        Console.ReadKey();
+        Console.ReadKey();
+    }
+}
